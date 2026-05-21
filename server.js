538654d935a1621967a964f6853ca4002903c531
안선영 <sunyoung@anseon-yeong-ui-MacBookPro.local>
2026-05-21 18:40:19 +0900

    fix: 캐시 버스팅 + no-cache 헤더 추가

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const path    = require('path');
 const app  = express();
 const PORT = process.env.PORT || 3456;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+  etag: false,
+  lastModified: false,
+  setHeaders: (res) => {
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  }
+}));
 
 app.listen(PORT, () => {
   console.log(`뭐먹.zip running on port ${PORT}`);
